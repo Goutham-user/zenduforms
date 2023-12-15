879da3c023956fa76cf6a854597579f6fed476e5
goutham375 <goutham375@gmail.com>
2023-12-15 12:18:54 +0530

    updated status and svg

--- src/app/components/map-viewer/map-viewer.component.ts
+++ src/app/components/map-viewer/map-viewer.component.ts
@@ -11,35 +11,35 @@ export class MapViewerComponent {
       location: 'Airport',
       from: 'airport@hyd.com',
       to: 'airport@mumbai.com',
-      completed: true,
+      status: 'Completed',
       dueDate: '06 December'
     },
     {
       location: 'Nagole Metro Station',
       from: 'metro@hyd.com',
       to: 'metro@mumbai.com',
-      completed: true,
+      status: 'Uncompleted',
       dueDate: '06 December'
     },
     {
       location: 'Hi-Tech City Metro Station',
       from: 'metro@hyd.com',
       to: 'metro@mumbai.com',
-      completed: false,
+      status: 'Low Risk',
       dueDate: '16 December'
     },
     {
       location: 'LB Nagar Metro Station',
       from: 'metro@hyd.com',
       to: 'metro@mumbai.com',
-      completed: false,
+      status: 'Normal Review',
       dueDate: '26 December'
     },
     {
       location: 'Raidurg Metro Station',
       from: 'metro@hyd.com',
       to: 'metro@mumbai.com',
-      completed: false,
+      status: 'Completed',
       dueDate: '05 January'
     }
   ]
